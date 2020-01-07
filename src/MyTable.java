82686d82515fc274af356798f5fd2c057dbf1a80
vathanak <vathanakkongsothybtb35@gmail.com>
2020-01-07 21:13:38 +0700

    update project

--- src/MyTable.java
+++ src/MyTable.java
@@ -2,38 +2,38 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyTable extends JFrame {
-    JTable tb;
-    MyTable(){
-        super("Table demo");
-        setSize(400,350);
-        setLayout(new FlowLayout());
-        tb = new JTable(10,4);
-        add(tb);
-        tb.setValueAt("name",0,0);
-        tb.setValueAt("Sax",0,1);
-        tb.setValueAt("Age",0,2);
-        tb.setValueAt("Score",0,3);
-
-        tb.setValueAt("Dara",1,0);
-        tb.setValueAt("Male",1,1);
-        tb.setValueAt("23",1,2);
-        tb.setValueAt("69",1,3);
-
-        tb.setValueAt("sokhom",2,0);
-        tb.setValueAt("Male",2,1);
-        tb.setValueAt("22",2,2);
-        tb.setValueAt("87",2,3);
-
-        tb.setValueAt("Dany",3,0);
-        tb.setValueAt("Female",3,1);
-        tb.setValueAt("21",0,2);
-        tb.setValueAt("97",0,3);
-
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public static void main(String[] args) {
-        new MyTable();
-    }
+//    JTable tb;
+//    MyTable(){
+//        super("Table demo");
+//        setSize(400,350);
+//        setLayout(new FlowLayout());
+//        tb = new JTable(10,4);
+//        add(tb);
+//        tb.setValueAt("name",0,0);
+//        tb.setValueAt("Sax",0,1);
+//        tb.setValueAt("Age",0,2);
+//        tb.setValueAt("Score",0,3);
+//
+//        tb.setValueAt("Dara",1,0);
+//        tb.setValueAt("Male",1,1);
+//        tb.setValueAt("23",1,2);
+//        tb.setValueAt("69",1,3);
+//
+//        tb.setValueAt("sokhom",2,0);
+//        tb.setValueAt("Male",2,1);
+//        tb.setValueAt("22",2,2);
+//        tb.setValueAt("87",2,3);
+//
+//        tb.setValueAt("Dany",3,0);
+//        tb.setValueAt("Female",3,1);
+//        tb.setValueAt("21",0,2);
+//        tb.setValueAt("97",0,3);
+//
+//        setVisible(true);
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    }
+//
+//    public static void main(String[] args) {
+//        new MyTable();
+//    }
 }
--- src/OOP/test2.java
+++ src/OOP/test2.java
@@ -1,4 +1,7 @@
 package OOP;
 
 public class test2 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
